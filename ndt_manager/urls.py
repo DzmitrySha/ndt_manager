e07a47b15517e6093da0b18261b01dedb0e54021
DzmitrySha <ravenbydev@gmail.com>
2023-02-26 22:54:29 +0300

    add media path for view files in debug mode

--- ndt_manager/urls.py
+++ ndt_manager/urls.py
@@ -1,6 +1,7 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-# from ndt_manager import settings
+from ndt_manager import settings
 from ndt_manager.views import IndexView, LoginUser, LogoutUser
 
 urlpatterns = [
@@ -16,3 +17,6 @@ urlpatterns = [
     path('certificates/', include('certificates.urls')),
     path('select2/', include('django_select2.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
